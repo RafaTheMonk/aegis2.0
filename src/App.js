40fc1b3bb27809738d9e5fa485d62874fd29d40a
Rafael <rafaelbritovaladares@gmail.com>
2023-11-28 01:20:21 -0300

    Componente Category e conceito de filter no array

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import Footer from './components/Footer/index';
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from './components/Card/index';
-import videos from "./json/iniciante.json"
-import videosIntermediario from "./json/intermediario.json"
-import videosAvancados from "./json/avancado.json"
+import videos from "./json/videos.json"
+import Category from "./components/Category";
+
+const categories = [
+  "Nível iniciante",
+  "Nível intermediário",
+  "Nível avançado"
+]
+
+function filterCategory(id){
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function App() {
   return (
@@ -13,18 +22,15 @@ function App() {
       <Header />
       <Banner image="favoritos" />
       <Container>
-        <h2>Nível Iniciante:</h2>
-        <section className="cards">
-        { videos.map((video) => <Card id={video.id} key={video.id}/>) }
-        </section>
-        <h2>Nível Intermediário:</h2>
-        <section className="cards">
-        { videosIntermediario.map((video) =><Card id={video.id} key={video.id}/>) }
-        </section>
-        <h2>Nível Avançado:</h2>
-        <section className="cards">
-        { videosAvancados.map((video) =><Card id={video.id} key={video.id}/>) }
-        </section>
+        <Category category={categories[0]}>
+          { filterCategory(0).map((video)=> <Card id={video.id} key={video.id} />)}
+        </Category>
+        <Category category={categories[1]}>
+          { filterCategory(1).map((video)=> <Card id={video.id} key={video.id} />)}
+        </Category>
+        <Category category={categories[2]}>
+          { filterCategory(2).map((video)=> <Card id={video.id} key={video.id} />)}
+        </Category>
       </Container>
       <Footer />
     </>
