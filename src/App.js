77084efcbdaef346f72dde20c2b3dfbed5270729
Rafael <rafaelbritovaladares@gmail.com>
2023-11-28 08:01:25 -0300

    Solução de mapeamento, export, import e escopo

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import Footer from './components/Footer/index';
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from './components/Card/index';
-import videos from "./json/videos.json"
-import Category from "./components/Category";
-
-const categories = [
-  "Nível iniciante",
-  "Nível intermediário",
-  "Nível avançado"
-]
-
-function filterCategory(id){
-  return videos.filter( video => video.category === categories[id] )
-}
+import Category, { categories, filterCategory } from "./components/Category/index.js";
 
 function App() {
   return (
@@ -22,15 +11,15 @@ function App() {
       <Header />
       <Banner image="favoritos" />
       <Container>
-        <Category category={categories[0]}>
-          { filterCategory(0).map((video)=> <Card id={video.id} key={video.id} />)}
-        </Category>
-        <Category category={categories[1]}>
-          { filterCategory(1).map((video)=> <Card id={video.id} key={video.id} />)}
-        </Category>
-        <Category category={categories[2]}>
-          { filterCategory(2).map((video)=> <Card id={video.id} key={video.id} />)}
-        </Category>
+        
+        {
+          categories.map((category, index)=>
+            <Category category={category}>
+              { filterCategory(index).map((video)=> <Card id={video.id} key={video.id} />)}
+            </Category>
+          )
+        }
+
       </Container>
       <Footer />
     </>
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,4 +1,15 @@
 import styles from "./Category.module.css"
+import videos from "../../json/videos.json";
+
+export const categories = [
+    "Nível iniciante",
+    "Nível intermediário",
+    "Nível avançado"
+]
+  
+export function filterCategory(id){
+    return videos.filter( video => video.category === categories[id] )
+}
 
 function Category({ category, children }) {
     return (
